e086a0b4c780b59f5b5b14928c270b1a5f9853e7
Duy Pham <duypn@kalapa.vn>
2021-10-27 18:18:42 +0700

    [master@0] : setup project

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, {Fragment, useEffect, useState} from "react";
+
+import axios from "axios";
+
+function App() {
+
+    const [prePriceChange, setPrePriceChange] = useState([]);
+
+    const [currentPriceChange, setCurrentPriceChange] = useState([]);
+
+    const [resultData, setResultData] = useState([]);
+
+    const [timestamp, setTimestamp] = useState(Date.now());
+
+    useEffect(() => {
+        Notification.requestPermission().then();
+
+        setInterval(() => {
+            setTimestamp(Date.now());
+        }, 1000*60*5);
+    }, []);
+
+    useEffect(() => {
+        var notificationMessage = '###BINANCE MONITOR###\r\r';
+
+        var data = prePriceChange.sort((a, b) => {
+            return Math.abs(currentPriceChange.filter(k => b.symbol === k.symbol)[0].priceChangePercent - b.priceChangePercent) - Math.abs(currentPriceChange.filter(k => a.symbol === k.symbol)[0].priceChangePercent - a.priceChangePercent)
+        }).slice(0, 5).map((i, iIndex) => {
+            let value = Math.abs(currentPriceChange.filter(j => i.symbol === j.symbol)[0].priceChangePercent - i.priceChangePercent).toFixed(3);
+            notificationMessage += i.symbol + '\r' + '\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t' + value + (iIndex !== 4 ? '\r\r' : '');
+            return {
+                symbol: i.symbol,
+                value: value
+            }
+        });
+
+        setResultData(data);
+
+        new Notification(notificationMessage);
+    }, [currentPriceChange]);
+
+    useEffect(() => {
+        getData();
+    }, [timestamp]);
+
+    function getData() {
+        axios.get('https://www.binance.com/fapi/v1/ticker/24hr').then((response) => {
+            let priceChange = response.data.map(i => ({
+                symbol: i.symbol,
+                priceChangePercent: i.priceChangePercent
+            }));
+
+            if (prePriceChange.length === 0) {
+                setPrePriceChange(priceChange);
+            } else {
+                setPrePriceChange(currentPriceChange);
+            }
+
+            setCurrentPriceChange(priceChange);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src="https://gcdn.pbrd.co/images/MXho7UMaA6te.png" className="App-logo" alt="logo" style={{
+                    filter: 'hue-rotate(15deg)'
+                }}/>
+                <h3>Binance Monitor</h3>
+                {React.useMemo(() => (resultData.length !== 0) && <div className="row" style={{
+                    padding: '0 32px'
+                }}>
+                    {resultData.map((i, iIndex) => <Fragment key={iIndex}>
+                        <div className="col-6 text-start">{i.symbol}</div>
+                        <div className="col-6 text-end">{i.value}</div>
+                    </Fragment>)}
+                </div>, [timestamp, resultData])}
+            </header>
+        </div>
+    );
+}
+
+export default App;
